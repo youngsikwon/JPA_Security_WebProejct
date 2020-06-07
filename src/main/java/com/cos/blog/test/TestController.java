1454f99163e16e6e420c344d7cf47fef56fe0d0e
youngsik_won <youngsik3200@gmail.com>
2020-06-08 01:04:45 +0900

    spring test

---
+++ src/main/java/com/cos/blog/test/TestController.java
@@ -0,0 +1,1 @@
+package com.cos.blog.test;import org.springframework.web.bind.annotation.GetMapping;import org.springframework.web.bind.annotation.RestController;@RestControllerpublic class TestController {   @GetMapping("/test/test")   public String test() {      return "test?";   }}
