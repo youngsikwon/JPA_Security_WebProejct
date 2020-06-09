32b6a62c416dca8ab863bc3deb7f0e1ad8e80554
youngsik_won <youngsik3200@gmail.com>
2020-06-10 03:03:23 +0900

    Table 생성 튜토리얼 교육용

---
+++ src/main/java/com/cos/blog/test/TempControolerTest.java
@@ -0,0 +1,1 @@
+package com.cos.blog.test;import org.springframework.stereotype.Controller;import org.springframework.web.bind.annotation.GetMapping;@Controllerpublic class TempControolerTest {   //http://localhost:8000/blog/test/temp   @GetMapping("/test/temp")   public String tempTest(){      return "home.jsp";   }}
