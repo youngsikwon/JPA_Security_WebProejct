a426cda85f6fd8135c02288bea62b0af36c4a945
youngsik_won <youngsik3200@gmail.com>
2020-07-07 22:24:03 +0900

    summer note APi 활용

---
+++ src/main/java/com/cos/blog/repository/BoardRepository.java
@@ -0,0 +1,1 @@
+package com.cos.blog.repository;import com.cos.blog.model.Board;import org.springframework.data.jpa.repository.JpaRepository;import java.util.Optional;public interface BoardRepository extends JpaRepository<Board, Integer>{}
