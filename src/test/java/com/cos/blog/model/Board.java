ee75f67efb49eaa9c1e5c5127a5b95aad40866c9
youngsikwon <youngsik3200@gmail.com>
2021-01-15 02:50:00 +0900

    Board Table 설정 완료

--- src/test/java/com/cos/blog/model/Board.java
+++ src/test/java/com/cos/blog/model/Board.java
@@ -1,4 +1,31 @@
 package com.cos.blog.model;
 
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
 public class Board {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)// auto_increament
+    private int id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Lob // 대용 데이터
+    private String content; //섬머노트 라이브<html>태그 섞인 라이
+
+    @ColumnDefault("0")
+    private int count;
+
+    @ManyToOne // Many = board & User = One
+    @JoinColumn(name = "userId")
+    private User user; //DB -> Object 저장X (FX, Java는 저장 가능)
+
+    @CreationTimestamp
+    private Timestamp createDate;
 }
