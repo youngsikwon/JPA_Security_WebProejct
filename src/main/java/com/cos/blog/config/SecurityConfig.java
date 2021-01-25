d775460cc2fd332794fc828752e61caae7e65bfc
youngsikwon <youngsik3200@gmail.com>
2021-01-25 10:41:25 +0900

    시큐리티 커스터마이징 - 완료  - /auth 통일
     - 로그인 사용할 시 해시암호 작업 중
     -

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -1,13 +1,29 @@
 package com.cos.blog.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration //빈등록 IOC
+@EnableWebSecurity // 시큐리티 필터가 등록이 된다.
+@EnableGlobalMethodSecurity(prePostEnabled = true) //특정 주소로 접근하면 권한 및 인증을 미리 체크함.
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+    http
+            .authorizeRequests()
+            .antMatchers("/auth/**")
+            .permitAll()
+            .anyRequest()
+            .authenticated()
+            .and()
+                .formLogin()
+            .loginPage("/auth/loginForm");// UserController -> login method
+
     }
+    //
 }
 
