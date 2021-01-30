da1cc5a24f0811f06ff82f867a6dba7e6c456d32
youngsikwon <youngsik3200@gmail.com>
2021-01-30 18:36:56 +0900

    kakao Token 값 설정 중

--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -54,4 +54,10 @@ public class UserService {
 		// 영속화된 persistance 객체의 변화가 감지되면 더티체킹이 되어 update문을 날려줌.
 	}
 
+	@Transactional(readOnly = true)
+	public User 회원찾기(String username){
+		User user =  userRepository.findByUsername(username).get();
+		return user;
+	}
+
 }
